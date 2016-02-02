f4557f76c60835ac6ac5d1af30575f6bc7086c1f
smusings <etyulmenkov@yahoo.com>
2016-02-01 19:37:08 -0500

    Get values from a Map

--- chart.js
+++ chart.js
@@ -1,15 +1,66 @@
-var data = [1, 1, 2, 3, 5, 8, 13];
-var body = d3.select("body");
-var x    = d3.scale.linear()
-			.domain([0, d3.max(data)])
-			.range([0, 420]);
+var map  = {"burger": 
+				{ "sold":1000,
+					"amount":200000,
+					"details" : 
+					{
+						"bacon burger":{
+							"sold":500,
+							"amount":100000 },
+						"cheese burger":{
+							"sold":500,
+							"amount":100000}
+					}
+				},
+			"fries": 
+				{ "sold":500,
+					"amount":100000,
+					"details" : 
+					{
+						"bacon burger":{
+							"sold":250,
+							"amount":50000 },
+						"cheese burger":{
+							"sold":250,
+							"amount":50000}
+					}
+				},
+			"shakes": 
+				{ "sold":300,
+					"amount":10000,
+					"details" : 
+					{
+						"bacon burger":{
+							"sold":150,
+							"amount":5000 },
+						"cheese burger":{
+							"sold":150,
+							"amount":5000}
+					}
+				}
+			};
 
-d3.select(".chart")
-	.selectAll("div")
-	.data(data)
-	.enter().append("div")
-	.style("width", function(d)
-	{
-		return x(d) + "px";
-	})
-	.text(function(d) { return d});
+function drawGraph(map, key) {
+	var data = [];
+
+	for (var i in map) {
+  		if (map.hasOwnProperty(i)) {
+			data.push(map[i][key]);
+		}
+ 	}
+
+	var x = d3.scale.linear()
+		.domain([0, d3.max(data)])
+		.range([0, 420]);
+
+	d3.select(".chart")
+		.selectAll("div")
+		.data(data)
+		.enter().append("div")
+		.style("width", function(d)
+		{
+			return x(d) + "px";
+		})
+		.text(function(d) { return d});
+}
+
+drawGraph(map, "sold");
